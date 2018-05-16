266cc29e44aaaca537bd77e97d9253b647d651ef
Steven Young (s5078722) <s5078722@bournemouth.ac.uk>
2018-05-16 11:56:28 +0100

    attempted to create the bell movement script

---
+++ Assets/Bell_movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bell_movement : MonoBehaviour
+{
+    public static bool hit;
+    private void Update()
+    {
+        
+        if (hit) {transform.Translate(Vector3.up *0.125f); }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "seesawplank")
+        {
+            hit = true;
+        }
+
+        while (collision.gameObject.name == "cube123")
+        {
+            hit = false;
+        }
+
+    }
+   
+
+
+
+
+
+
+}
---
+++ Assets/boxBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "bell")
+        {
+            Bell_movement.hit = false;
+        }
+
+
+    }
+}
