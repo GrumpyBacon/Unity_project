94391755b643329661f2a9d078425e5676ed60ba
Steven Young (s5078722) <s5078722@bournemouth.ac.uk>
2018-05-18 15:08:20 +0100

    created the force for the ball to go round the loop

---
+++ Assets/script/Thrust_ball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust_ball : MonoBehaviour {
+
+    public float speed;
+
+
+
+    // Use this for initialization
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+        if (collision.gameObject.tag == "hammer") {
+
+          
+            Debug.Log("hammer");
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * speed);
+
+
+
+
+        }
+
+
+
+        
+
+
+
+
+
+    }
+
+
+    
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+}
---
+++ Assets/script/Thust_trigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thust_trigger : MonoBehaviour {
+
+    public float speed;
+
+
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.tag == "hammer")
+        {
+
+
+            Debug.Log("hammer");
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * speed);
+
+            
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
