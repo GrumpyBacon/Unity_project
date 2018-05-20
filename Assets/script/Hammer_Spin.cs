6d9b1cb2bef6d3d292495889891f34b626ab940f
Steven Young (s5078722) <s5078722@bournemouth.ac.uk>
2018-05-20 19:16:29 +0100

    all devices have now been complete with a test of physics 5 out of 5 times the ball lands in the desired place, now to move on to finishing the camera to follow the route of the machine

---
+++ Assets/script/Hammer_Spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer_Spin : MonoBehaviour {
+
+ 
+    void start ()
+    {
+        HingeJoint hinge = GetComponent<HingeJoint>();
+        JointMotor motor = hinge.motor;
+        motor.force = 1000000;
+        motor.targetVelocity = -1000000;
+        motor.freeSpin = true;
+        hinge.motor = motor;
+        hinge.useMotor = true;
+    }
+}
---
+++ Assets/script/On_collision_turnOffScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class On_collision_turnOffScript : MonoBehaviour {
+
+
+    public GameObject scriptedObject;
+
+    public GameObject colliderObject;
+
+
+    void OnCollisionEnter(Collision collision)
+
+    {
+
+
+
+
+
+
+        if (gameObject.gameObject == colliderObject)
+        {
+
+            scriptedObject.GetComponent<Hammer_Spin>();
+            enabled = false;
+
+
+
+
+
+
+        }
+
+
+
+
+        //if (collision.gameObject == colliderObject)
+        {
+           // Destroy(scriptedObject);
+        }
+
+
+    }
+
+
+
+
+
+}
